7c77d71d8d6a99d748d103bb1a868863aaf05c0f
KricsDavid <david@kricsfalusi.hu>
2024-06-05 08:16:47 +0200

    Add project files.

---
+++ Bingo/BingoJatekos.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bingo
+{
+    internal class BingoJatekos
+    {
+        public string Nev { get; set; }
+        public int[,] Kartya { get; set; }
+        public bool[,] Talalatok { get; set; }
+
+        public BingoJatekos(string nev, int[,] kartya)
+        {
+            Nev = nev;
+            Kartya = kartya;
+            Talalatok = new bool[5, 5];
+        }
+    }
+}
---
+++ Bingo/MainWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Bingo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+
+        public List<BingoJatekos> Beolvasas()
+        {
+            List<BingoJatekos> jatekosok = new List<BingoJatekos>();
+
+            string[] files = Directory.GetFiles("forras", "*.txt");
+            foreach (string file in files)
+            {
+                string nev = System.IO.Path.GetFileNameWithoutExtension(file);
+                int[,] kartya = BeolvasKartya(file);
+                BingoJatekos jatekos = new BingoJatekos(nev, kartya);
+                jatekosok.Add(jatekos);
+            }
+
+            return jatekosok;
+        }
+
+        private int[,] BeolvasKartya(string file)
+        {
+            int[,] kartya = new int[5, 5];
+            string[] lines = File.ReadAllLines(file);
+            for (int i = 0; i < 5; i++)
+            {
+                string[] values = lines[i].Split(';');
+                for (int j = 0; j < 5; j++)
+                {
+                    kartya[i, j] = int.Parse(values[j]);
+                }
+            }
+            return kartya;
+        }
+
+
+
+
+
+
+    }
+
+
+
+}
